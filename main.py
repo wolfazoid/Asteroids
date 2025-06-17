8af9ce920b8081f25a39872304a00a743f5a77ae
wolfazoid <wolfahoward@gmail.com>
2025-06-16 22:38:51 -0500

    Imported pygame, configured screen settings based on hardcoded width and height, added gitignore file, set up constants file.

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill('black')
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
