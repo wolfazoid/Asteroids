a8fb38b7856bad7edbf71a2a803fe6f4f62b3962
wolfazoid <wolfahoward@gmail.com>
2025-06-19 13:10:07 -0500

    added asteroid field, collision detection, and initial shooting mechanic.

--- main.py
+++ main.py
@@ -1,18 +1,58 @@
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_Field = AsteroidField()
+
+    Shot.containers = (shots, updatable,drawable)
+
+    Player.containers = (updatable, drawable)
+    
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    
     while True:
-        screen.fill('black')
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
+        for asteroid in  asteroids:
+            if player.detect_collision(asteroid):
+                print("Game over!")
+                sys.exit()
+
+
+        screen.fill('black')
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+        
+        # 60 FPS
+        dt = clock.tick(60) / 1000
+        
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__":
     main()
